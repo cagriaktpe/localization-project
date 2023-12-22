802c2563b22e4d8c73c4d0bbdb1cef13bd736360
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-22 13:31:30 +0300

    Initial Commit

---
+++ LocalizationProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizationProject
+//
+//  Created by Samet Çağrı Aktepe on 22.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizationProject/LocalizationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationProjectApp.swift
+//  LocalizationProject
+//
+//  Created by Samet Çağrı Aktepe on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
